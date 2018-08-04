95c56dd735341190ed27b39775d2af4172d36e62
Pl4cek <pl4cek@outlook.com>
2018-08-04 21:09:48 +0200

    Nawigowanie wstecz i do następnej strony

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -13,12 +13,12 @@ const checkoutSummary = (props) => {
 			</div>
 			<Button
 				btnType='Success'
-				clicked>
+				clicked ={props.checkoutContinued}>
 				CONTINUE
 			</Button>
 			<Button
 				btnType='Danger'
-				clicked>
+				clicked = {props.checkoutCancelled}>
 				CANCEL
 			</Button>
 		</div>
